33cee0053a43d998aafe08ff4b2633168b66e546
Eliyas Varghese <eliyasvarghese97@gmail.com>
2023-02-08 22:09:06 +0530

    updates some files with simple programmes

--- python/demo.py
+++ python/demo.py
@@ -1,1 +1,12 @@
-# this is a demo file
+# this is a demo py programme
+
+legs = input('enter no of legs ')
+
+if legs == '8':
+  print(' oh its a spider.')
+elif legs == '4':
+  print('its an animal')
+elif legs == '2':
+  print('it is human')
+else :
+  print('i d k what it is, may be an reptile or alien')
--- python/new.py
+++ python/new.py
@@ -1,3 +1,18 @@
 
-# simple program
-print("hello world")
+# a simple program to check if you are eligible for vaccination
+
+age = int(input("Enter you age: "))
+
+if age >= 18:
+  are_u_pregnent = input("are you pregnent [y/n]:")
+  if are_u_pregnent == 'y' or are_u_pregnent =='Y':
+        print("You're not eligible to  vaccination")
+  else : 
+         
+    taken_first_dose = input("Have you taken the first dose covid vaccine (y/n): ")
+    if taken_first_dose == 'y' or taken_first_dose == 'Y':
+        print("You're eligible to second dose vaccine")
+    else:
+        print("Visit your nearest vaccine centre first dose vaccine")
+else:
+    print("You are minor, Keep using mask and soap")
